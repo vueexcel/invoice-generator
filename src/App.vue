8d6ded808ca87a99e7c38ea277d56b46c11790c8
vueexcel <vue.excel2011@gmail.com>
2023-08-28 11:19:32 +0530

    create a invoice generator chrome extension

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import HelloWorld from './components/InvoiceGenerator.vue'
+
+</script>
+
+<template>
+ 
+    <div class="wrapper">
+      <HelloWorld/>
+    </div>
+  
+</template>
+
+<style scoped>
+ .wrapper {
+    background: white;
+  }
+
+@media (min-width: 1024px) {
+   .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
